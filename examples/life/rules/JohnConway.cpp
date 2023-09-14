f6b78db944c4664141c704ce04fea794142e6562
hannah1590 <hannah.fasco@mymail.champlain.edu>
2023-09-14 14:13:02 -0400

    made some changes

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -6,5 +6,15 @@ void JohnConway::Step(World& world) {
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
+  int size = world.SideSize();
 
+  bool live = world.Get(point);
+  bool up;
+
+  if(point.y + 1 < world.SideSize())
+     up = world.Get(Point2D(point.x,point.y + 1));
+  else if(point.y + 1 >= world.SideSize())
+  {
+     up = world.Get(Point2D(point.x,point.y));
+  }
 }
