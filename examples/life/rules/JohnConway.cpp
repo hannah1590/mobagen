b24d522f0ba851ce837213c58c56eb4dfad6a167
hannah1590 <hannah.fasco@mymail.champlain.edu>
2023-09-18 14:18:24 -0400

    worked a little on life

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -9,56 +9,72 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   int size = world.SideSize();
 
   bool live = world.Get(point);
-  bool up;
-  bool down;
-  bool right;
-  bool left;
-  bool upRight;
-  bool upLeft;
-  bool downRight;
-  bool downLeft;
+
+  int count;
 
   // currently done copying programming assignment where the map
   // started at the top left and increasing a number would move
   // the point right/down
 
-  // up
-  if(point.y + 1 < size)
-     up = world.Get(Point2D(point.x,point.y + 1));
-  else if(point.y + 1 >= size)
-     up = world.Get(Point2D(0,point.y));
+  // cells directly next to each other
+  if (point.y + 1 < size && world.Get(Point2D(point.y + 1,point.x)))
+    count++;
+  if (point.y - 1 >= 0 && world.Get(Point2D(point.y - 1, point.x)))
+    count++;
+  if (point.x + 1 < size && world.Get(Point2D(point.y, point.x + 1)))
+    count++;
+  if (point.x - 1 >= 0 && world.Get(Point2D(point.y, point.x - 1)))
+    count++;
 
-  // down
-  if(point.y - 1 >= 0)
-     down = world.Get(Point2D(point.x,point.y - 1));
-  else if(point.y - 1 < 0)
-     down = world.Get(Point2D(world.SideSize() - 1,point.y));
+  // cells diagonal from each other
+  if (point.y + 1 < size && point.x + 1 < size && world.Get(Point2D(point.y + 1, point.x + 1)))
+    count++;
+  if (point.y + 1 < size && point.x - 1 >= 0 && world.Get(Point2D(point.y + 1, point.x - 1)))
+    count++;
+  if (point.y - 1 >= 0 && point.x + 1 < size && world.Get(Point2D(point.y - 1, point.x + 1)))
+    count++;
+  if (point.y - 1 >= 0 && point.x - 1 >= 0 && world.Get(Point2D(point.y - 1, point.x - 1)))
+    count++;
 
-  // right
-  if(point.x + 1 < size)
-     right = world.Get(Point2D(point.x + 1,point.y));
-  else if(point.x - 1 >= size)
-     right = world.Get(Point2D(point.x, 0));
+  // cells next to each other with the continuous map
+  if (point.x + 1 >= size && world.Get(Point2D(point.y, 0)))
+    count++;
+  if (point.x - 1 < 0 && world.Get(Point2D(point.y, size - 1)))
+    count++;
+  if (point.y + 1 >= size && world.Get(Point2D(0, point.x)))
+    count++;
+  if (point.y - 1 < 0 && world.Get(Point2D(size - 1, point.x)))
+    count++;
 
-  // left
-  if(point.x - 1 >= 0)
-     left = world.Get(Point2D(point.x - 1,point.y));
-  else if(point.x - 1 < 0)
-     left = world.Get(Point2D(point.x,size - 1));
+  // diagonally with continuous map off right/bottom
+  if (point.x + 1 >= size && point.y + 1 < size && world.Get(Point2D(point.y + 1, 0)))
+    count++;
+  if (point.x + 1 >= size && point.y - 1 >= 0 && world.Get(Point2D(point.y - 1, 0)))
+    count++;
+  if (point.y + 1 >= size && point.x + 1 < size && world.Get(Point2D(0, point.x + 1)))
+    count++;
+  if (point.y + 1 >= size && point.x - 1 >= 0 && world.Get(Point2D(0, point.x - 1)))
+    count++;
 
-  // upRight
-  if(point.x + 1 < size && point.y - 1 >= 0)
-     upRight = world.Get(Point2D(point.x + 1,point.y - 1));
+  // diagonally with continuous map off left/top
+  if (point.x - 1 < 0 && point.y + 1 < size && world.Get(Point2D(point.y + 1, size - 1)))
+    count++;
+  if (point.x - 1 < 0 && point.y - 1 >= 0 && world.Get(Point2D(point.y - 1, size - 1)))
+    count++;
+  if (point.y - 1 < 0 && point.x + 1 < size && world.Get(Point2D(size - 1, point.x + 1)))
+    count++;
+  if (point.y - 1 < 0 && point.x - 1 >= 0 && world.Get(Point2D(size - 1, point.x - 1)))
+    count++;
 
-  // upLeft
-  if(point.x - 1 >= 0 && point.y - 1 >= 0)
-     upRight = world.Get(Point2D(point.x - 1,point.y - 1));
+  // diagonally with continuous map off diagonals
+  if (point.x + 1 >= size && point.y + 1 >= size && world.Get(Point2D(0, 0)))
+    count++;
+  if (point.x + 1 >= size && point.y - 1 < 0 && world.Get(Point2D(size - 1, 0)))
+    count++;
+  if (point.x - 1 < 0 && point.y + 1 >= size && world.Get(Point2D(0, size - 1)))
+    count++;
+  if (point.x - 1 < 0 && point.y - 1 < 0 && world.Get(Point2D(size - 1, size - 1)))
+    count++;
 
-  // downRight
-  if(point.x + 1 < size && point.y + 1 < size)
-     upRight = world.Get(Point2D(point.x + 1,point.y + 1));
-
-  // downLeft
-  if(point.x - 1 >= 0 && point.y + 1 < size)
-     upRight = world.Get(Point2D(point.x - 1,point.y + 1));
+  return count;
 }
